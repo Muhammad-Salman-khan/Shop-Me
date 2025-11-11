9f2764bcbe01d387efac322e238e55027c2920cb
Muhammad-Salman-khan <alibinkhan465@gmail.com>
2025-11-11 16:10:32 +0500

    fix: enable changeOrigin for API proxy configuration in Vite

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
       "/api": {
         target: "http://localhost:5000",
         rewrite: (path) => path.replace(/^\/api/, ""),
+        changeOrigin: true,
       },
     },
   },
